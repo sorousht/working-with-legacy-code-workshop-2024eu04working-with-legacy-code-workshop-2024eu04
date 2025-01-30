dc66b4bdd1cc4c8e6ca11d1c4edf97ccea671de9
Markus Roesch <markus.roesch@sap.com>
2025-01-30 05:52:48 +0100

    small adjustments in the spacing of the menu text

--- constants.js
+++ constants.js
@@ -9,14 +9,13 @@ const TEMPLATE = "$fullName\r\n" +
 
 
 const DISPLAY = "\r\n" +
-"Jam Invitation and Page Generator\r\n" +
+"  Jam Invitation and Page Generator\r\n" +
 "  --------------------------------- \r\n" +
 "0. Exit\r\n" +
-"1. Create aJam Invitations For Employees & Jam Group XYZ \r\n" +
-"2. Create Jam Invitations For Managers & Jam Group XYZ' \r\n" +
-"3. Create Member Overview Page \r\n";
-
-
+"1. Create Jam Invitations For Employees & Jam Group XYZ using CSV \r\n" +
+"2. Create Jam Invitations For Employees & Jam Group XYZ using DB \r\n" +
+"3. Create Jam Invitations For Managers & Jam Group XYZ' \r\n" +
+"4. Create Member Overview Page \r\n";
 
 module.exports = {
     TEMPLATE,
