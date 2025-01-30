639e315b6f7a794f46cb7115d99332b0bb7743d6
Markus Roesch <markus.roesch@sap.com>
2025-01-30 05:53:10 +0100

    allow to choose between db and csv usage

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
 const Menu = require('./Menu');
 const FileManager = require('./FileManager');
-// const EmployeeFileService = require('./EmployeeFileService');
+const EmployeeFileService = require('./EmployeeFileService');
 const EmployeeDatabaseService = require('./EmployeeDatabaseService');
 const InvitationGenerator = require('./InvitationGenerator');
 const { TEMPLATE } = require("./constants");
 
-managers = function() {
+managers = function () {
 };
 
-memberList = function() {
+memberList = function () {
 };
 
-const menu = new Menu();
 
-const invitationGenerator = new InvitationGenerator(TEMPLATE);
+main();
 
-// do {
-    menu.display();
-    const input = menu.getInput();
-    switch(input.choice) {
-        case '0': break;
-        case '1': 
-            EmployeeDatabaseService.loadEmployees().then(employees => {
+async function main() {
+    const menu = new Menu();
+    const invitationGenerator = new InvitationGenerator(TEMPLATE);
+
+    do {
+        menu.display();
+        menu.mainMenuQuestion();
+        switch (menu.choice) {
+            case '0': break;
+            case '1': {
+                const dataSource = menu.filePathQuestion();
+                const employees = EmployeeFileService.loadEmployees(dataSource);
                 FileManager.writeFile('EmployeeInvitations.txt', invitationGenerator.generate(employees));
-            });
-            break;
-        case '2':
-            managers();
-            break;
-        case '3': 
-            memberList();
-            break;
-        default:
-            console.log('Please enter a number within the range listed above.');
-    }
-// } while(menu.choice>0);
-console.log();
-console.log('Goodbye!');
+                break;
+            }
+            case '2': {
+                const employees = await EmployeeDatabaseService.loadEmployees();
+                FileManager.writeFile('EmployeeInvitations.txt', invitationGenerator.generate(employees));
+                break;
+            }
+            case '3':
+                managers();
+                break;
+            case '4':
+                memberList();
+                break;
+            default:
+                console.log('Please enter a number within the range listed above.');
+        }
+    } while (menu.choice > 0);
+
+    console.log();
+    console.log('Goodbye!');
+}
+
